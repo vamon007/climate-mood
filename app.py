945cc50adf7dedcab78ac2249c913fdbf73b0b01
Aakash Desai <aakashdesai@aakashs-air.attlocal.net>
2019-02-11 17:51:10 -0800

    Add css and tweet box

--- app.py
+++ app.py
@@ -25,25 +25,45 @@ def get_df_from_db():
 
 tdf = get_df_from_db()
 app.layout = html.Div(children=[
-    html.H1(children='Climate Moods'),
+    html.Div(children=[
+        html.H1(children='Climate Moods'),
 
-    html.Div(children='''
-             Twitter sentiment analysis on climate change.
-    '''),
+        html.Div(children=['''
+             Twitter sentiment analysis on climate change
+        '''], className='description'),
+    ], className='navigation'),
 
-    dcc.Graph(
-        id='line',
-        figure={
-            'data': [
-                {'x': tdf.TimeStamp, 'y': tdf["Polarity"], 
-                 'type': 'basic-line',},    
-            ],
-            'layout': {
-                'title': 'Polarity Trend of the Tweets'
+    html.Div(children=[
+
+        html.Div(children=[
+            html.H3(children=['Tweet Sentiment vs. Time']),
+            html.Div(id='tweet-content')
+        ], className='senti-title'),
+        dcc.Graph(
+            id='sentiment-chart',
+            figure={
+                'data': [
+                    {'x': tdf.TimeStamp, 'y': tdf["Polarity"], 
+                     'type': 'basic-line',
+                     'customdata': tdf["Polarity"]
+                }],
+                'layout': {
+                }
             }
-        }
-    )
+        )
+    ], className='sentiment')
 ])
+
+@app.callback(
+    dash.dependencies.Output('tweet-content', 'children'),
+    [dash.dependencies.Input('sentiment-chart', 'hoverData')])
+def update_text(hoverData):
+    s = tdf[tdf["Polarity"] == hoverData['points'][0]['customdata']]
+    return html.P(
+        '{}'.format(
+            s.iloc[0]["Tweets"]
+        )
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
